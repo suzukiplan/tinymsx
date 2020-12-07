64ad5d736c45bdb45dcb03eacd91635478c7d90b
suzukiplan <suzukiplan.ys@gmail.com>
2020-12-07 18:44:22 +0900

    load rom file & make TinyMSX instance

--- test/cli/test.cpp
+++ test/cli/test.cpp
@@ -2,5 +2,22 @@
 
 int main(int argc, char* argv[])
 {
+    if (argc < 2) {
+        puts("usage: tinymsx rom-file");
+        return 1;
+    }
+    FILE* fp = fopen(argv[1], "rb");
+    if (!fp) {
+        puts("File not found");
+        return 1;
+    }
+    void* rom;
+    fseek(fp, 0, SEEK_END);
+    size_t romSize = ftell(fp);
+    rom = malloc(romSize);
+    fseek(fp, 0, SEEK_SET);
+    fread(rom, 1, romSize, fp);
+    fclose(fp);
+    TinyMSX msx(TINY_MSX_TYPE_MSX1, rom, romSize, TINY_MSX_COLOR_MODE_RGB555);
     return 0;
 }
