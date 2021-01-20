3ca6d9d931bc580733f6c06a0cac4e63fdc420a4
suzukiplan <suzukiplan.ys@gmail.com>
2021-01-21 00:33:38 +0900

    correct default mega rom type

--- test/osx/EmuBoard/EmuBoard.c
+++ test/osx/EmuBoard/EmuBoard.c
@@ -43,7 +43,7 @@ static int getTypeOfRom(char* rom, size_t romSize)
 {
     if (0x8000 < romSize) {
         puts("open as MSX1 ASC8 mega-rom file");
-        return TINYMSX_TYPE_MSX1_ASC8X;
+        return TINYMSX_TYPE_MSX1_ASC8;
     } else if (64 * 1024 == romSize) {
         puts("open as MSX1 64KB ROM file");
         return TINYMSX_TYPE_MSX1;
